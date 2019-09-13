59292d310a78dd4f222385832effadbe11a6eeb2
Gustavo <gustavo.avila@softexpert.com>
2019-09-13 18:11:24 -0300

    develop

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -6,19 +6,19 @@
 # Copyright 2019 -  All rights reserved.
 # *******************************************************************************************
 
-ROOT_FOLDER=/opt/pentaho/data-integration/autoDeploy
+WORK_FOLDER=/opt/pentaho/data-integration/autoDeploy
 DEPLOY_ADDRESS=10.0.2.15
 DEPLOY_PORT=8080
 USER=cluster
 PWD=cluster
+CONFIG_FILE=client1.deploy
 
-# /*** OBTER INFORMACAO DE CONFIGURACAO DO REPOSTIORIO ***/
-#curl -H "Accept:application/vnd.github.v3.raw" -L https://raw.github.com/SoftExpertIntegrations/OnPremise-Config/master/client1.deploy --output $ROOT_FOLDER/client1.deploy
-#wget --spider --http-user=cluster --http-password=cluster0908 http://52.67.35.163/kettle/executeJob/?job=$job
+# /*** OBTER INFORMACAO DE CONFIGURACAO DO DEPLOY ***/
+curl -H "Accept:application/vnd.github.v3.raw" -L https://raw.github.com/SoftExpertIntegrations/OnPremise-Config/master/$CONFIG_FILE --output $WORK_FOLDER/$CONFIG_FILE
 
-#while read p; do
-#	    curl -H 'Accept:application/vnd.github.v3.raw' --remote-name -L $p
-#done < client1.deploy
+while read p; do
+    curl -H 'Accept:application/vnd.github.v3.raw' --remote-name -L $p
+done < $CONFIG_FILE
 
 for i in $(ls -C1)  
 do
@@ -32,7 +32,14 @@ do
 			<safe_mode>N</safe_mode> 
 			</transformation_execution_configuration> 
 			</transformation_configuration>" >> $i.xml
-			curl -X POST -H "Content-Type: application/json"  -u $USER:$PWD -d @$i.xml http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/registerTrans/?xml=Y  >> autoDeployTrans.log
+
+			curl -X POST -H "Content-Type: application/x-www-form-urlencoded"  -u $USER:$PWD -d @$i.xml http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/registerTrans/?xml=Y  >> autoDeployTrans.log
+			#sleep 5
+			#echo basename -s .ktr "$i" >
+			#echo "$i" | cut -f 1 -d '.'
+			#echo $name
+			#echo ${i##.ktr/}
+			#echo $trans_name 
 			;;
 		*.kjb)
 			cp $i  $i.xml
@@ -42,44 +49,35 @@ do
 			<safe_mode>N</safe_mode> 
 			</job_execution_configuration> 
 			</job_configuration>" >> $i.xml
-			curl -X POST -H "Content-Type: application/json"  -u $USER:$PWD -d @$i.xml http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/registerJob/?xml=Y >> autoDeployJobs.log
+
+			#echo curl -X POST -H "Content-Type: application/json"  -u $USER:$PWD -d @$i.xml http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/registerJob/?xml=Y >> autoDeployJobs.log				
 			;;
 	esac
 done
 
-find . -name "*.xml" -type f -delete
-find . -name "*.ktr" -type f -delete
-find . -name "*.kjb" -type f -delete	
-find . -name "*.deploy" -type f -delete
-#while read p; do
+while read i; do
+	case $i in
+		*.ktr)
+			file_name="${i##*/}"
+			TRANS_NAME="${file_name%.*}"
+			#echo curl -X POST -H "Content-Type: application/x-www-form-urlencoded"  -u $USER:$PWD http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/prepareExec/?name=$TRANS_NAME >> autoDeployTrans.log
+			#sleep 5
+			#echo curl -X POST -H "Content-Type: application/x-www-form-urlencoded"  -u $USER:$PWD http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/startExec/?name=$TRANS_NAME  >> autoDeployTrans.log
+			;;
+		*.kjb)
+			file_name="${i##*/}"
+			JOB_NAME="${file_name%.*}"			
+			#echo curl -X POST -H "Content-Type: application/json"  -u $USER:$PWD http://$DEPLOY_ADDRESS:$DEPLOY_PORT/kettle/startJob/?name=$JOB_NAME >> autoDeployJobs.log	
+			;;
+	esac
+done < $CONFIG_FILE
 
-#	echo $p >> $i.xml
-	#if [$first_line=1] ; then
-        #if [[ $first_line=1 ]] ; then
-	#	echo "<transformation_configuration>"  >> $i.xml
-	#	first_line=0
-	#fi
-	
-#done < $i
 
-#while read p; do
 
-#  case $p in
-#	*.ktr)
-#		echo "Case 1 "$p
 
-#		;;
-#	*.kjb)
-#		echo "Case 2 "$p
-		#break
-#		;;
-#  esac
 
-#done < client1.deploy
+#find . -name "*.xml" -type f -delete
+#find . -name "*.ktr" -type f -delete
+#find . -name "*.kjb" -type f -delete	
+#rm -f $CONFIG_FILE
 
-#if [[ $p =~ ".ktr" ]] then
-#   echo $p
-#fi
-#if [[ $p =~ ".kjb" ]] then
-#   echo $p
-#fi
