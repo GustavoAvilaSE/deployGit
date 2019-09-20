1c7b00397a6394c6e16c6f7b41a520222c1d458c
GustavoAvilaSE <gustavo.avila@softexpert.com>
2019-09-20 09:55:25 -0300

    Implement config.bat
    Implement config.bat to storage configurable variables  separate to code

--- workStation/updateRepo.bat
+++ workStation/updateRepo.bat
@@ -8,14 +8,15 @@ REM ****************************************************************************
 
 REM *** ASK GITHUB REPOSITORY NAME ***
 REM SET /P GITHUB_REPOSITORY_NAME=GITHUB REPOSITORY NAME:
-SET GIT_DISPLAY_NAME=testRepo
-SET GIT_EMAIL=gustavo.avila@softexpert.com
+REM SET GIT_DISPLAY_NAME=testRepo
+REM SET GIT_EMAIL=gustavo.avila@softexpert.com
+
+call config.bat
 
 setlocal
-set ROOT_FOLDER=%~dp0
-set SOURCE_FOLDER=%~dp0source
 
 cd /D %SOURCE_FOLDER%
+
 SET REPO_EXIST= 0
 REM setlocal EnableDelayedExpansion
 
@@ -27,27 +28,30 @@ for /r "%SOURCE_FOLDER%" %%x in (.) do (
      )
 )
 :break
+echo %REPO_EXIST%
 
 REM *** VERIFIES IF IS A NEW REPOSITORY OR AN EXISTING ONE ***
-IF /I %REPO_EXIST% EQU 1 (
+IF /I %REPO_EXIST% EQU 0 (
     REM *** CREATE LOCAL REPOSITORY AND ASOCIATE IT A REMOTE GITHUB REPOSITORY ***
     git init 
-    git config --local user.name %GIT_DISPLAY_NAME%
+    git config --local user.name %GIT_USER_NAME%
     git config --local user.email %GIT_EMAIL%
-    REM git remote add origin  https://github.com/SoftExpertIntegrations/%GITHUB_REPOSITORY_NAME%  *** -> REAL ORGANIZATION
-    REM git remote add origin  https://github.com/SoftExpertIntegrations/%GITHUB_REPOSITORY_NAME% 
-    git remote add origin https://github.com/GustavoAvilaSE/testRepo
+    git remote add origin  https://github.com/%GITHUB_ORGANIZATION%/%GITHUB_REPOSITORY_NAME% 
     git pull origin master
     REM git config --local credential.helper "store --file %~dp0/deploy/config/gitcredential"
-    git config --local credential.helper 
     git add .
     git commit -m "Initial Upload from Consultor Workstation"
     git push origin master
 ) ELSE (
     REM *** MAKE A PULL, ADD AND COMMIT FORM LOCAL REPOSITORY TO A REMOTE GITHUB REPOSITORY ***   
-    git config --local user.name %GIT_DISPLAY_NAME%
+    git config --local user.name %GIT_USER_NAME%
     git config --local user.email %GIT_EMAIL%
     git pull origin master
     git add .
     git commit -m "Update sources from Consultor Workstation"
-    git push origin master)
+    git push origin master
+)
+
+
+
+
